86f0cf45b1d6aa8f31d7c7ee7e5b2743f78755ad
Азиза Амануллина <zizowca@gmail.com>
2022-01-02 23:00:39 +0300

    Добавьте файлы проекта.

---
+++ Задание 18/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string one = Console.ReadLine();
+            Console.WriteLine(Check(one));
+            Console.ReadKey();
+        }
+
+        static bool Check (string one)
+        {
+            Stack<char> stack = new Stack<char>();
+            Dictionary<char, char> sk = new Dictionary<char, char>
+                {
+                    {'(',')'},
+                    {'{','}'},
+                    {'[',']'},
+                };
+            foreach (char c in one)
+            {
+                if (c == '(' || c == '{' || c == '[')
+                    stack.Push(sk[c]);
+                if (c == ')' || c == '}' || c == ']')
+                {
+                    if (stack.Count == 0 || stack.Pop() != c)
+                    {
+                        return false;
+                    }
+                }
+            }
+                if (stack.Count == 0)
+                    return true;
+                else
+                    return false;  
+        }
+
+    }
+}
